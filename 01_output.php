ff9d9e61fc33cba1ea3c2021bc895dd4777ad2c3
ksbm009 <kevinstephenbiju2003@gmail.com>
2025-01-03 00:04:15 +0530

    testing 01_output.php with HTML structure and additional PHP content

--- 01_output.php
+++ 01_output.php
@@ -5,3 +5,21 @@ Hello, World! x1
 ?>
 <br>
 Hi, World!
+<br>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo '123'; ?></h1>
+</body>
+</html>
+
+<?php
+    echo "Playing around with PHP";
+    //Single line Comment
+    /* Multiple line Comment */
