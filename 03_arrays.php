3b14d39eca8e8c5d1620457b9270d3c326019133
ksbm009 <kevinstephenbiju2003@gmail.com>
2025-01-06 23:57:20 +0530

    add multi-dimensional array examples in 03_arrays.php; create 04_conditionals.php with operators and if statements

--- 03_arrays.php
+++ 03_arrays.php
@@ -31,3 +31,25 @@
     ];
 
     echo $person['first_name'];
+
+    // Multi-dimensional Array
+    $people = [
+        [
+            'first_name' => "Kevin",
+            'last_name' => "Stephen Biju",
+            'email' => "kevinstephenbiju2003@gmail.com"
+        ],
+        [
+            'first_name' => "Ryan",
+            'last_name' => "Reji John",
+            'email' => "ryanrejijohn@gmail.com"
+        ],
+        [
+            'first_name' => "Alexa",
+            'last_name' => "Bobby",
+            'email' => "alexabobby@gmail.com"
+        ]
+    ];
+
+    echo $people[1]['email'];
+    var_dump(json_encode($people));
---
+++ 04_conditionals.php
@@ -0,0 +1,40 @@
+<?php
+    /* ------------ Operators ----------- */
+
+    /*
+      < Less than
+      > Greater than
+      <= Less than or equal to
+      >= Greater than or equal to
+      == Equal to
+      === Identical to
+      != Not equal to
+      !== Not identical to
+    */
+
+    /* ---------- If Statements --------- */
+
+    /*
+    ** If Statement Syntax
+    if (condition) {
+      // code to be executed if condition is true
+    }
+    */
+
+    $age = 21;
+    if ($age >= 18) {
+        echo "You are eligible to vote.";
+    } else {
+        echo "Sorry, You are not eligible to vote.";
+    }
+
+    $t = date("H");
+    echo $t;
+
+    if ($t < "12") {
+        echo "Good Morning!";
+    } else if($t < "17") {
+        echo "Good Afternoon!";
+    } else {
+        echo "Good Evening!";
+    }
