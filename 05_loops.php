90696dad15248ebc207cf06cfd737f0c673f29dc
ksbm009 <kevinstephenbiju2003@gmail.com>
2025-01-08 23:39:10 +0530

    add foreach loop examples and syntax comments in 05_loops.php

--- 05_loops.php
+++ 05_loops.php
@@ -43,3 +43,35 @@
         echo "The number is : $z <br>";
         $z++;
     } while ($z <= 5);
+
+    /* ---------- Foreach Loop ---------- */
+
+    /*
+    ** Foreach Loop Syntax
+      foreach ($array as $value) {
+      // code to be executed
+      }
+    */
+
+    $posts = ['First Post', 'Second Post', 'Third Post'];
+
+    for($x = 0; $x < count($posts); $x++) {
+        echo $posts[$x] . "<br>";
+    }
+
+    foreach($posts as $post) {
+        echo $post . "<br>";
+    }
+
+    foreach($posts as $index => $post) {
+        echo $index + 1 . " - " . $post . "<br>";
+    }
+
+    $person = [
+        'Name' => 'Kevin Stephen',
+        'Age' => 21,
+    ];
+
+    foreach($person as $key => $value) {
+        echo $key . " - " . $value . "<br>";
+    }
