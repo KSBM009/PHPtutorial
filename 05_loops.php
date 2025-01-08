e506aba8ac8c6b24e12b778c4a2fa278960246ea
ksbm009 <kevinstephenbiju2003@gmail.com>
2025-01-08 23:23:15 +0530

    add for loop implementation and while loop syntax comments in 05_loops.php

--- 05_loops.php
+++ 05_loops.php
@@ -7,4 +7,16 @@
       // code to be executed
       }
     */
+    for($x=0; $x<=10; $x++) {
+        echo "The number is: $x <br>";
+    }
+
+    /* ------------ While Loop ------------ */
+
+    /*
+    ** While Loop Syntax
+      while (condition) {
+      // code to be executed
+      }
+    */
     
