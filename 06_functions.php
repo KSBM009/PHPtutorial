dff63724544f0ffd034a5e9bc5c123555d9f667b
ksbm009 <kevinstephenbiju2003@gmail.com>
2025-01-09 23:45:13 +0530

    add user registration functions and a sum function in 06_functions.php

--- 06_functions.php
+++ 06_functions.php
@@ -1,2 +1,24 @@
 <?php
     
+    $x = 10;
+    
+    function registerUser(){
+        global $x;
+        echo $x . "<br>";
+        echo "User Registered Successfully!";
+    }
+
+    registerUser();
+
+    function register_User($username){
+        echo "User $username Registered Successfully!";
+    }
+
+    register_User('ksbm009');
+
+    function sum($a, $b){
+        return $a + $b;
+    }
+
+    echo sum(10, 20);
+    
