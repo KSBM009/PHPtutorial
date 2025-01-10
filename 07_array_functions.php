8916396a433bfde75c7cd255ca244dd439b752b0
ksbm009 <kevinstephenbiju2003@gmail.com>
2025-01-10 23:52:07 +0530

    add array manipulation examples in 07_array_functions.php

--- 07_array_functions.php
+++ 07_array_functions.php
@@ -1,2 +1,29 @@
 <?php
-    
+    $fruits = ['apple', 'banana', 'orange', 'grape', 'pear'];
+
+    // Get the number of elements in an array
+    echo count($fruits) . "<br>";
+
+    // Search for a value in an array
+    echo var_dump(in_array('apple', $fruits)) . "<br>";
+    echo var_dump(in_array('apples', $fruits)) . "<br>";
+
+    // Add an item to the array
+    $fruits[] = 'pineapple';
+    array_push($fruits, 'kiwi');
+    array_push($fruits, 'watermelon', 'papaya');
+    array_unshift($fruits, 'mango');
+
+    print_r($fruits);
+
+    // Remove an item from the array
+    array_pop($fruits);
+    array_shift($fruits);
+    unset($fruits[1]);
+
+    print_r($fruits);
+
+    // Split it into different chunks
+    $chunked = array_chunk($fruits, 3);
+
+    print_r($chunked);
