ab830b32602ec4a15f894e22a439dc2416d6817f
ksbm009 <kevinstephenbiju2003@gmail.com>
2025-01-12 23:51:39 +0530

    add string manipulation examples in 08_string_functions.php

--- 08_string_functions.php
+++ 08_string_functions.php
@@ -1,3 +1,15 @@
 <?php
 
+    /*
+      Functions to work with strings
+      https://www.php.net/manual/en/ref.strings.php
+    */
+
     $string = "Hello, World!";
+
+    // Get the length of a string
+    echo strlen($string) . "<br>";
+
+    // Find the position of the first occurrence of a substring in a string
+    echo strpos($string, 'World') . "<br>";
+    
