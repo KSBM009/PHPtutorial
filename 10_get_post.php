64c62f6d824b710c9c5a8546ac1880f75c873e18
ksbm009 <kevinstephenbiju2003@gmail.com>
2025-01-15 23:47:26 +0530

    add example usage of $_GET superglobal in 10_get_post.php

--- 10_get_post.php
+++ 10_get_post.php
@@ -4,5 +4,12 @@
     /*
       We can pass data through urls and forms using the $_GET and $_POST superglobals.
     */
+    
+    echo $_GET['name'];
+    echo $_GET['age'];
 
 ?>
+
+<a href="<?php echo $_SERVER['PHP_SELF']; ?>?name=Stephen&age=75">Click</a>
+
+<form action=""></form>
