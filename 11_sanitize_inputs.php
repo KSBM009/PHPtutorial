84e38f1c7f49c266c505fff6f218775cf5e4464c
ksbm009 <kevinstephenbiju2003@gmail.com>
2025-01-20 23:52:57 +0530

    refactor: sanitize user input in 11_sanitize_inputs.php and add new 12_cookies.php file

--- 11_sanitize_inputs.php
+++ 11_sanitize_inputs.php
@@ -9,14 +9,32 @@
     // echo $_GET['age'];
 
     if(isset($_POST['submit'])){
-        $name = htmlentities($_POST['name']);
-        $age = htmlentities($_POST['age']);
+        
+        // $name = htmlentities($_POST['name']);
+        // $age = htmlentities($_POST['age']);
+
+        // $name = htmlspecialchars($_POST['name']);
+        // $age = htmlspecialchars($_POST['age']);
+
+        $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+        $age = filter_input(INPUT_POST, 'age', FILTER_SANITIZE_SPECIAL_CHARS);
+
+        // $name = filter_var($_POST['name'], FILTER_SANITIZE_SPECIAL_CHARS);
+        // $age = filter_var($_POST['age'], FILTER_SANITIZE_SPECIAL_CHARS);
+
         echo $name . ' is ' . $age . ' years old.';
+
+        // FILTER_SANITIZE_STRING - Convert string to string with only alphanumeric, whitespace, and the following characters - _.:/
+        // FILTER_SANITIZE_EMAIL - Convert string to a valid email address
+        // FILTER_SANITIZE_URL - Convert string to a valid URL
+        // FILTER_SANITIZE_NUMBER_INT - Convert string to an integer
+        // FILTER_SANITIZE_NUMBER_FLOAT - Convert string to a float
+        // FILTER_SANITIZE_FULL_SPECIAL_CHARS - HTML-encodes special characters, keeps spaces and most other characters
     }
 
 ?>
 
-<a href="<?php echo $_SERVER['PHP_SELF']; ?>?name=Stephen&age=75">Click</a>
+<a href="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>?name=Stephen&age=75">Click</a>
 
 <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get">
     <div>
