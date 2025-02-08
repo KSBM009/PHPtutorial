156165e1bc524074585b38e29aa2a628d34d284c
ksbm009 <kevinstephenbiju2003@gmail.com>
2025-02-08 23:18:08 +0530

    feat: implement file reading functionality and add user list in extras/users.txt

--- 14_file_handling.php
+++ 14_file_handling.php
@@ -5,4 +5,11 @@
       File handling is the ability to read and write files on the server.
       PHP has built in functions for reading and writing files.
     */
-    
+
+    $file = 'extras/users.txt';
+
+    if(file_exists($file)){
+        echo readfile($file);
+    } else {
+        echo "File does not exist";
+    }
