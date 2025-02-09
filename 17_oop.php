8b0d30951c52635867943c79a1eccb34bed86a05
ksbm009 <kevinstephenbiju2003@gmail.com>
2025-02-09 23:54:38 +0530

    feat: add initial implementation of Object Oriented Programming concepts in 17_oop.php

---
+++ 17_oop.php
@@ -0,0 +1,16 @@
+<?php
+    /* ---------- Object Oriented Programming --------- */
+
+    /* 
+      Object Oriented Programming (OOP) is a programming paradigm that uses objects and classes.
+      It aims to implement real-world entities like inheritance, polymorphism, encapsulation, etc.
+      in the programming.
+    */
+
+    /*
+      From PHP5 onwards you can write PHP in either a procedural or object oriented way. 
+      OOP consists of classes that can hold "properties" and "methods". 
+      Objects can be created from classes.
+    */
+    
+?>
